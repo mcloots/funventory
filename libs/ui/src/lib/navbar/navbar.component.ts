a94af9b0a7908ee3d6de66c3bb6ec832f4ad3e7e
Michaël Cloots <michael.cloots@thomasmore.be>
2024-03-05 20:46:45 +0100

    flowbite + tailwind installed

---
+++ libs/ui/src/lib/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mcloots-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {}
